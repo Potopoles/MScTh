73229ce796cac2a15d6709d87a35e180d8034bce
heimc <christoph.heim@env.ethz.ch>
2019-04-25 14:55:33 +0200

    Updated copy scripts.

--- copy/copy_fields.sh
+++ copy/copy_fields.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
 
-#cp /project/pr04/heimc/initial_data/wl_4.4_km/4_lm_f/output/zlev/lff*z.nc \
-#    $SCRATCH/MScTh/01_rawData/4.4
-#cp /project/pr04/heimc/initial_data/wl_4.4_km_filtered/4_lm_f/output/zlev/lff*z.nc \
-#    $SCRATCH/MScTh/01_rawData/4.4f
-#cp /project/pr04/heimc/initial_data/wl_2.2_km/4_lm_f/output/zlev/lff*z.nc \
-#    $SCRATCH/MScTh/01_rawData/2.2
-#cp /project/pr04/heimc/initial_data/wl_2.2_km_filtered/4_lm_f/output/zlev/lff*z.nc \
-#    $SCRATCH/MScTh/01_rawData/2.2f
-#cp /project/pr04/heimc/initial_data/wl_1.1_km/4_lm_f/output/zlev/lff*z.nc \
-#    $SCRATCH/MScTh/01_rawData/1.1
-#cp /project/pr04/heimc/initial_data/wl_1.1_km_filtered/4_lm_f/output/zlev/lff*z.nc \
-#    $SCRATCH/MScTh/01_rawData/1.1f
-#
-#
-#exit 1
+src_path=/project/pr04/heimc/data/cosmo_out/MScTh
+cp $src_path/RAW4/4_lm_f/output/zlev/lff*z.nc \
+    $SCRATCH/MScTh/01_rawData/4.4
+cp $src_path/SM4/4_lm_f/output/zlev/lff*z.nc \
+    $SCRATCH/MScTh/01_rawData/4.4f
+cp $src_path/RAW2/4_lm_f/output/zlev/lff*z.nc \
+    $SCRATCH/MScTh/01_rawData/2.2
+cp $src_path/SM2/4_lm_f/output/zlev/lff*z.nc \
+    $SCRATCH/MScTh/01_rawData/2.2f
+cp $src_path/RAW1/4_lm_f/output/zlev/lff*z.nc \
+    $SCRATCH/MScTh/01_rawData/1.1
+cp $src_path/SM1/4_lm_f/output/zlev/lff*z.nc \
+    $SCRATCH/MScTh/01_rawData/1.1f
 
 
 
@@ -24,34 +22,34 @@
 #srcPath=/scratch/snx1600/heimc/MScTh/02_fields/diurnal
 #srcPath=/scratch/snx1600/heimc/MScTh/02_fields/topocut
 #srcPath=/project/pr04/heimc/initial_data/move
-srcPath=/project/pr04/heimc/02_fields/diurnal
+#srcPath=/project/pr04/heimc/02_fields/diurnal
 
-destPath=$SCRATCH/MScTh/02_fields/diurnal
+#destPath=$SCRATCH/MScTh/02_fields/diurnal
 #destPath=$SCRATCH/02_fields/topocut
 #destPath=/scratch/snx3000/heimc/MScTh/02_fields/diurnal
 #destPath=/scratch/snx3000/heimc/MScTh/02_fields/topocut
 
-var=nTOT_PREC.nc
-
-echo $var
-
-echo 4.4
-cp $srcPath/4.4/$var $destPath/4.4/
-echo 4.4f
-cp $srcPath/4.4f/$var $destPath/4.4f/
-echo 2.2
-cp $srcPath/2.2/$var $destPath/2.2/
-echo 2.2f
-cp $srcPath/2.2f/$var $destPath/2.2f/
-echo 1.1
-cp $srcPath/1.1/$var $destPath/1.1/
-echo 1.1f
-cp $srcPath/1.1f/$var $destPath/1.1f/
-
-echo 4.4r
-cp $srcPath/4.4r/$var $destPath/4.4r/
-echo 2.2r
-cp $srcPath/2.2r/$var $destPath/2.2r/
-echo 1.1r
-cp $srcPath/1.1r/$var $destPath/1.1r/
-
+#var=nTOT_PREC.nc
+#
+#echo $var
+#
+#echo 4.4
+#cp $srcPath/4.4/$var $destPath/4.4/
+#echo 4.4f
+#cp $srcPath/4.4f/$var $destPath/4.4f/
+#echo 2.2
+#cp $srcPath/2.2/$var $destPath/2.2/
+#echo 2.2f
+#cp $srcPath/2.2f/$var $destPath/2.2f/
+#echo 1.1
+#cp $srcPath/1.1/$var $destPath/1.1/
+#echo 1.1f
+#cp $srcPath/1.1f/$var $destPath/1.1f/
+#
+#echo 4.4r
+#cp $srcPath/4.4r/$var $destPath/4.4r/
+#echo 2.2r
+#cp $srcPath/2.2r/$var $destPath/2.2r/
+#echo 1.1r
+#cp $srcPath/1.1r/$var $destPath/1.1r/
+#
---
+++ copy/copy_project_scratch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp -r /project/pr04/heimc/02_fields/* ../02_fields
+cp -r /project/pr04/heimc/01_rawData/* ../01_rawData 
+
+
+
+
